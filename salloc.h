d88a16ef078a6c45a9ebb78e0c690f1acda4e000
jhkst <jhkst@users.noreply.github.com>
2025-05-19 21:56:42 +0200

    Update salloc.h

--- salloc.h
+++ salloc.h
@@ -2,9 +2,11 @@
 #define _SALLOC_H
 
 #include <stdlib.h>
+#include <string.h>
 
 void handle_exit();
 
 void *salloc(size_t size, void (*error_handle)());
+char *sstrdup(const char *s, void (*error_handle)());
 
 #endif
